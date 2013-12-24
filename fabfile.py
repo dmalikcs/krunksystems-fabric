d8180390519fc2dfc6c16c3728ed6a5b4bd2d446
dmalikcs <dmalikcs@gmail.com>
2013-12-24 15:42:39 +0530

    djp_setup defined

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,8 @@
 from fabric.api import local
 
 
-def setup(project_name):
-    print project_name
-
-def hello():
-    local("ls")
+def djp_setup(project_name, destination="/home/dmalik5/django-dev/"):
+    destination = "/home/dmalik5/django-dev/%s/" % project_name
+    local("mkdir %s" % destination)
+    local("django-admin.py startproject --template=https://github.com/dmalikcs/django-project-template/archive/master.zip  %s %s" % (project_name, destination))
+    local("git init %s" % destination)
